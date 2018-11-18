bbe7081bbf832fbca736f973de01a93169f8a8ea
Dmitriy <11lione1991@gmail.com>
2018-11-18 16:09:42 +0200

    add percentComplete

--- progress.js
+++ progress.js
@@ -1,2 +1,42 @@
-var A = 10;
-console.log( A )
+function log(html) {
+	document.getElementById('log').innerHTML = html;
+}
+
+console.log( document.body )
+var file = document.body.getElementById('fileItem').files[0];
+
+
+document.forms.upload.onsubmit = function() {
+
+	var file = this.elements.myfile.files[0];
+	if (file) {
+		upload(file);
+	}
+	return false;
+}
+
+function upload(file) {
+
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("POST", "http://localhost:8000/upload", true);
+	open(method, url, async, user, password)
+	xhr.send(file);
+
+	xhr.onload = xhr.onerror = function() { // обработка запроса
+		if (this.status === 200) {
+			log("success");
+		} else {
+			log("error " + this.status);
+		}
+	};
+
+	if (e.lengthComputable) { 
+		var percentComplete = (e.loaded / e.total) * 100; // это значение использовать для отображения progress bar 
+	}
+
+	xhr.upload.onprogress = function(event) {  	// обработчик для закачки , отображение количества загрузки в байтах
+		log(event.loaded + ' / ' + event.total);
+	}
+
+}
--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 const express = require('express');
-const fileUpload = require('express-fileupload');;
+const fileUpload = require('express-fileupload');
 const app = express();
 
 app.use('/form', express.static(__dirname + '/index.html'));
 app.use('/progress.js', express.static(__dirname + '/progress.js'));
 
 // default options
-app.use(fileUpload());
+app.use( fileUpload() );
 
-app.get('/ping', function(req, res) {
-  res.send('pong');
-});
+// app.get('/ping', function(req, res) {
+//   res.send('pong');
+// });
 
 app.post('/upload', function(req, res) {
   let sampleFile;
   let uploadPath;
+	console.log('req.files >>>', req.files); // eslint-disable-line
 
   if (Object.keys(req.files).length == 0) {
     res.status(400).send('No files were uploaded.');
     return;
   }
-
-  console.log('req.files >>>', req.files); // eslint-disable-line
 
   sampleFile = req.files.sampleFile;
 
