57e3c5fad461cca61f382005ad66e50413008544
Dmitriy <11lione1991@gmail.com>
2018-11-19 13:48:08 +0200

    add onsubmit function

--- progress.js
+++ progress.js
@@ -1,42 +1,60 @@
-function log(html) {
-	document.getElementById('log').innerHTML = html;
-}
+// function log(html) {
+// 	document.getElementById('log').innerHTML = html;
+// }
 
-console.log( document.body )
-var file = document.body.getElementById('fileItem').files[0];
+// console.log( window )
+// var file = document.getElementsByClassName('form-control-file')[0].files[0];
 
+document.forms.upload.onsubmit = function(e) { //обработчик отаправки формы
+	// console.log(e)
+	var formData = new FormData( document.forms )
 
-document.forms.upload.onsubmit = function() {
+	formData.append('Wonderful-image', document.getElementsByClassName('form-control-file')[0].files[0] )
 
-	var file = this.elements.myfile.files[0];
-	if (file) {
-		upload(file);
+	var xhr = new XMLHttpRequest()
+
+	xhr.open("POST", "http://localhost:8000/upload", false)
+
+	xhr.send( formData )
+
+	if ( xhr.status === 200 ) {
+		console.log('BLa')
+	} else {
+		console.log( xhr.status )
 	}
-	return false;
+
+	e.preventDefault();
 }
+
+
 
 function upload(file) {
+// отправка формы через FormData
 
-	var xhr = new XMLHttpRequest();
+	console.log(`file in upload`, file )
 
-	xhr.open("POST", "http://localhost:8000/upload", true);
-	open(method, url, async, user, password)
-	xhr.send(file);
+	// formData.append('image', file)
 
-	xhr.onload = xhr.onerror = function() { // обработка запроса
-		if (this.status === 200) {
-			log("success");
-		} else {
-			log("error " + this.status);
-		}
-	};
+	// var xhr = new XMLHttpRequest();
 
-	if (e.lengthComputable) { 
-		var percentComplete = (e.loaded / e.total) * 100; // это значение использовать для отображения progress bar 
-	}
+	// xhr.open("POST", "http://localhost:8000/upload", true);
+	
+	// xhr.send( formData );
 
-	xhr.upload.onprogress = function(event) {  	// обработчик для закачки , отображение количества загрузки в байтах
-		log(event.loaded + ' / ' + event.total);
-	}
+	// xhr.onload = xhr.onerror = function() { // обработка запроса 
+	// 	if (this.status === 200) {						// расделить обработку ошибки и успешное отправку файла
+	// 		log("success");											// обработка ошибки с сервера не через try catch
+	// 	} else {
+	// 		log("error " + this.status);
+	// 	}
+	// };
+
+	// if (e.lengthComputable) { 
+	// 	var percentComplete = (e.loaded / e.total) * 100; // это значение использовать для отображения progress bar 
+	// }
+
+	// xhr.upload.onprogress = function(event) {  	// обработчик для закачки , отображение количества загрузки в байтах
+	// 	log(event.loaded + ' / ' + event.total);
+	// }
 
 }
