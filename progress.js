e24addbff6c4334b9592ac8711dd8d030a8337ec
Dmitriy <11lione1991@gmail.com>
2018-11-19 20:11:42 +0200

    xhr with status 200

--- progress.js
+++ progress.js
@@ -1,60 +1,47 @@
-// function log(html) {
-// 	document.getElementById('log').innerHTML = html;
-// }
+document.forms.upload.onsubmit = function(e) { 					//обработчик отаправки формы
+// по дефолту при onsubmit происходит отправка формы и перезагрузка страницы
+	e.preventDefault() // отменяем перезагрузку страницы
+	
+	var formData = new FormData()
 
-// console.log( window )
-// var file = document.getElementsByClassName('form-control-file')[0].files[0];
-
-document.forms.upload.onsubmit = function(e) { //обработчик отаправки формы
-	// console.log(e)
-	var formData = new FormData( document.forms )
-
-	formData.append('Wonderful-image', document.getElementsByClassName('form-control-file')[0].files[0] )
+	formData.append('sampleFile', document.getElementsByClassName('form-control-file')[0].files[0] )
 
 	var xhr = new XMLHttpRequest()
 
-	xhr.open("POST", "http://localhost:8000/upload", false)
+	xhr.open("POST", 'http://localhost:8000/upload')
 
 	xhr.send( formData )
 
-	if ( xhr.status === 200 ) {
-		console.log('BLa')
-	} else {
-		console.log( xhr.status )
+	xhr.onreadystatechange = function(  ) { 
+
+		if ( this.status === 200 ) {
+			console.log( `this.upload`, this.upload )
+		} else {
+			console.log(`this.status`, this.status )
+		}
+
 	}
 
-	e.preventDefault();
 }
 
 
-
-function upload(file) {
-// отправка формы через FormData
-
-	console.log(`file in upload`, file )
-
-	// formData.append('image', file)
-
-	// var xhr = new XMLHttpRequest();
-
-	// xhr.open("POST", "http://localhost:8000/upload", true);
+// Свойство XMLHttpRequest.upload возвращает объект XMLHttpRequestUpload, 
+// представляющий процесс загрузки. Это непрозрачный объект, но так как он является XMLHttpRequestEventTarget, 
+// к нему можно добавить обработчики событий
 	
-	// xhr.send( formData );
+// formData.append( name(первый аргумент), file ) 
+// должен соответстовать с именем файла , 
+// когда input подвязан к форме, а когда мы его отвязали 
+// мы сами его задаём
 
-	// xhr.onload = xhr.onerror = function() { // обработка запроса 
-	// 	if (this.status === 200) {						// расделить обработку ошибки и успешное отправку файла
-	// 		log("success");											// обработка ошибки с сервера не через try catch
-	// 	} else {
-	// 		log("error " + this.status);
-	// 	}
-	// };
+// if (e.lengthComputable) { 
+// 	var percentComplete = (e.loaded / e.total) * 100; // это значение использовать для отображения progress bar 
+// }
 
-	// if (e.lengthComputable) { 
-	// 	var percentComplete = (e.loaded / e.total) * 100; // это значение использовать для отображения progress bar 
-	// }
+// xhr.upload.onprogress = function(event) {  	// обработчик для закачки , отображение количества загрузки в байтах
+// 	log(event.loaded + ' / ' + event.total);
+// }
 
-	// xhr.upload.onprogress = function(event) {  	// обработчик для закачки , отображение количества загрузки в байтах
-	// 	log(event.loaded + ' / ' + event.total);
-	// }
-
-}
+// function log(html) {
+// 	document.getElementById('log').innerHTML = html;
+// }
